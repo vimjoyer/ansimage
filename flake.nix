1bd4e9e1d6925895cd9178afae1aa49d4b86d94e
Yurii <ggooxxoorree@gmail.com>
2023-08-13 16:54:01 +0200

    added a flake

---
+++ flake.nix
@@ -0,0 +1,40 @@
+{
+  description = "ansimage flake";
+
+  inputs =
+    {
+      nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    };
+
+  outputs = { self, nixpkgs }:
+        let
+          system = "x86_64-linux";
+          pkgs = nixpkgs.legacyPackages.${system};
+
+          buildInputs = with pkgs; [
+            rustc
+            rustfmt
+            rust-analyzer
+            clippy
+            cargo
+            cargo-edit
+            cargo-watch
+            clang
+          ];
+
+        in
+        {
+          devShells.${system}.default =
+            pkgs.mkShell
+              {
+                buildInputs = buildInputs;
+              };
+
+          packages.${system}.default =
+            pkgs.rustPlatform.buildRustPackage {
+              name = "ansimg";
+              cargoLock.lockFile = ./Cargo.lock;
+              src = pkgs.lib.cleanSource ./.;
+            };
+        };
+}
